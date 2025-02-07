a907851e2a3196bb6ebf472cfe5327c1f476e656
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2025-02-07 13:11:16 +0530

    TASK 10157: Added code for first task for assignment - Added functionality to check for tsv zipped files in the pre-determined folder - Added functionality to iteratively load data from zipped tsv files to dataframe and concatenate them to resultant dataframe - Added functionality under dev for adding data from any new tsv zipped file added to the pre-determined folder to resultant dataframe

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from read_data import ReadData
+
+if __name__ == "__main__":
+    ReadData.folder_path = './Data'
+    ReadData.load_zipped_data()
---
+++ src/read_data.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy
+import os
+from pathlib import Path
+
+class ReadData:
+    folder_path = None
+
+    @classmethod
+    def concatenate_data(cls, files:list[str], resultant_dataframe:pd.DataFrame)->pd.DataFrame:
+        # read these files as dataframe and concatenate them to resultant dataframe
+        for file in files:
+            if file.endswith('.tsv.gz'):
+                data = pd.read_csv(f"{ReadData.folder_path}/{file}", compression='infer', header=None, on_bad_lines='skip', delimiter='\t')
+            else:
+                continue
+            print(f"Data from {ReadData.folder_path}/{file}")
+            ReadData.analyze_dataframe(data)
+            resultant_dataframe = pd.concat([resultant_dataframe, data], axis=0)
+            del data
+        
+        return resultant_dataframe
+    
+    @classmethod
+    def load_zipped_data(cls)->None:
+        # create empty dataframe
+        resultant_dataframe = pd.DataFrame()
+
+        # list the files in the data folder
+        files = os.listdir(ReadData.folder_path)
+
+        # read these files as dataframe and concatenate them to resultant dataframe
+        resultant_dataframe = ReadData.concatenate_data(files, resultant_dataframe)
+
+        # basic stats for resultant dataframe
+        if resultant_dataframe.empty == False:
+            ReadData.analyze_dataframe(resultant_dataframe)
+
+    @classmethod
+    def load_new_zipped_data(cls, stored_dataframe_file:str, used_files:str)->None:
+        # Read files list of data folder content
+        files_list = os.listdir(ReadData.folder_path)
+        
+        stored_data = None
+
+        # Check if the resultant dataframe file is stored in the data folder
+        if Path(f"./Data/{stored_dataframe_file}").exists() == True:
+            stored_data = pd.read_csv(f"{ReadData.folder_path}/{stored_dataframe_file}", header=None, on_bad_lines='skip', delimiter='\t')
+        else:
+            stored_data = pd.DataFrame()
+
+        # list of files that are already read and concatenated to resultant dataframe
+        read_files_list = []
+
+        # check if there is concatenated dataframe list file available 
+        if Path(f"./Data/{used_files}").exist() == False:
+            Path.touch(f"./Data/{used_files}")
+
+        # open the concatenated dataframe list file
+        with open(f"./Data/{used_files}", "r") as output:
+            read_files_list = list(output.read())
+        
+        # derive the list of files that are not concatenated in the resultant file
+        unread_list = [item for item in files_list if item not in read_files_list]
+
+        # append those new dataframes to resultant dataframe
+        for file in unread_list:
+            data = pd.read_csv(f"{ReadData.folder_path}/{file}", compression='infer', header=None, on_bad_lines='skip', delimiter='\t')
+            print(f"Data from {ReadData.folder_path}/{file}")
+            ReadData.analyze_dataframe(data)
+            stored_data = pd.concat([stored_data, data], axis=0)
+            del data
+
+        # show basic stats for the resultant dataframe
+        ReadData.analyze_dataframe(stored_data)
+
+    @classmethod
+    def analyze_dataframe(cls, data:pd.DataFrame)->None:
+        print(f"Resultant dataframe after concatenation:\n{data.head(10)}\ndataframe shape:{data.shape}\n")
+        print(f"Resultant dataframe stats:\n{data.describe()}")
