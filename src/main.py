e06e6cbdffe84880c5bb23c48c0a5a906d6e60f4
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2025-02-12 11:33:37 +0530

    TASK 10185: Implement following correct logic in the previous code - Split each  entry in the postproductlist into multiple products based on ','. - Create a separate dataframe and save it as zipped tsv file.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import gc
 if __name__ == "__main__":
     read_data = ReadData('./Data')
     read_data.load_zipped_data()
-    transform_data = TransformData(readData=read_data)
+    transform_data = TransformData(read_data=read_data)
     print(gc.collect())
-    filtered_data = transform_data.filter_rows_by_string(patternstr=r'20113')#regex for string match
+    filtered_data = transform_data.filter_rows_by_string(pattern_str=r'20113')#regex for string match
     dataframe_product_list = transform_data.create_productlist_dataframe(filtered_data)
--- src/transform_data.py
+++ src/transform_data.py
@@ -21,30 +21,11 @@ class TransformData:
 
     def create_productlist_dataframe(self, data_frame:pd.DataFrame)->pd.DataFrame:
         assert set(['postproductlist']).issubset(data_frame.columns), "Dataframe must contain postproductlist column in it"
-        data_frame.convert_dtypes(infer_objects=True) 
-        
-        product_data = data_frame.iloc[0, 2].split(';;;')
-        product_index = product_data[0]
-        product_features = [x for x in product_data[1].split('|')]
-        product_features_split = [x.split('=', 1) for x in product_features]
-        product_columns = [x[0] for x in product_features_split]
-        
-        dict_features = {}
-        
-        for a in data_frame.iloc[:, 2]:
-            product_data = a.split(';;;')
-            product_index = product_data[0]
-            product_features = [x for x in product_data[1].split('|')]
-            product_features_split = [x.split('=', 1) for x in product_features]
-            product_values =  [x[1] if len(x)>1 else '' for x in product_features_split]
+        data_frame.convert_dtypes(infer_objects=True)
 
-            dict_features.update({product_index: product_values})
+        resultant_dataframe = data_frame['postproductlist'].apply(lambda x: x.split(',')).explode()
 
-        resultant_dataframe = pd.DataFrame.from_dict(dict_features, orient='index', columns=product_columns)
-
-        print(f"post product list dataframe:\n{resultant_dataframe}")
-
-        resultant_dataframe.to_csv('postproductlist20113.tsv', sep='\t')
+        resultant_dataframe.to_csv('postproductlist20113.tsv.gz', sep='\t', compression='gzip')
         print(f"Dataframe saved as postproductlist20113.tsv")
 
         return resultant_dataframe
