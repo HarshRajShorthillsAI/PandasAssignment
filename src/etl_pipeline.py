ece126073d30273c331f911ad9ced41577c6aba5
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2025-02-23 03:24:06 +0530

    Added few changes in code to make the codebase more robust and reduce redundancy.

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -1,49 +1,33 @@
 from read_data import ReadData
 from transform_data import TransformData
-import os
-import gc
 
 class ETLPipeline:
     def __init__(self, folder_path:str):
-        self.read_data = ReadData(folder_path)
-        self.transform_data = TransformData(self.read_data)
+        self.read_data = ReadData(folder_path, False)
+        self.transform_data = TransformData(self.read_data, True)
 
     def task3(self):
-        read_data = ReadData('./Data')
-        read_data.load_zipped_data()
-        transform_data = TransformData(read_data=read_data)
-        transform_data.clean_dataframe()
-        transform_data.rename_dataframe_cols()
-        transform_data.filter_rows_by_string(pattern_str=r'20113')#regex for string match
-        transform_data.clean_dataframe()
-        transform_data.create_productlist_dataframe()
-        transform_data.store_dataframe_as_tsv()
+        self.read_data.load_zipped_data()
+        self.transform_data.rename_dataframe_cols()
+        self.transform_data.filter_rows_by_string(pattern_str=r'20113')
+        self.transform_data.create_productlist_dataframe()
+        self.transform_data.store_dataframe_as_tsv()
 
     def task4(self):
-        read_data = ReadData('./Data')
-        transform_data = TransformData(read_data=read_data)
-        transform_data.create_dealer_ad_impression_count_from_postproductlist()
+        self.transform_data.create_dealer_ad_impression_count_from_postproductlist()
 
     def task5(self):
-        read_data = ReadData('./Data')
-        transform_data = TransformData(read_data=read_data)
-        transform_data.count_impressions_groupby()
+        self.transform_data.count_impressions_groupby()
 
     def task6(self):
-        files = os.listdir(self.read_data.folder_path)
-
-        verbose = input("Do you want the details of read files from folder(0:No/1:Yes): ")
+        files = self.read_data.list_files()
 
         for file in files:
-            gc.collect()
             if file.endswith('.py'):
                 continue
             print(f"file: {file}")
-            self.transform_data.readData.resultant_dataframe = self.read_data.read_files(file, verbose=verbose)
-            self.transform_data.clean_dataframe()
+            self.transform_data.readData.resultant_dataframe = self.read_data.read_files(file)
             self.transform_data.filter_rows_by_string('20113')
             self.transform_data.create_productlist_dataframe()
             self.transform_data.create_dealer_ad_impression_count_from_postproductlist()
-            # self.transform_data.count_impressions_groupby()
-            self.transform_data.get_product_model_by_evar('117', 50000)
-            # del self.transform_data.readData.resultant_dataframe
+            self.transform_data.get_product_model_by_evar('117')
--- src/read_data.py
+++ src/read_data.py
@@ -5,36 +5,38 @@ from pathlib import Path
 
 class ReadData:
 
-    def __init__(self, folder_path:str):
+    def __init__(self, folder_path:str, verbose:bool):
         self.folder_path = folder_path
         self.resultant_dataframe = pd.DataFrame()
+        self.verbose = verbose
 
-    def read_files(self, filename:str, verbose:bool)->pd.DataFrame:
+    def read_files(self, filename:str)->pd.DataFrame:
         assert filename in os.listdir(self.folder_path), "File not found in directory path specified."
         data_frame = pd.read_csv(f"{self.folder_path}/{filename}", delimiter='\t', header=None, compression='infer', on_bad_lines='skip')
         self.analyze_dataframe(data_frame)
         return data_frame
 
-    def concatenate_data(self, data:pd.DataFrame, verbose:bool)->None:
+    def concatenate_data(self, data:pd.DataFrame)->None:
         self.resultant_dataframe = pd.concat([self.resultant_dataframe, data], axis=0, ignore_index=True)
-        print(f"dataframe memory: {self.resultant_dataframe.info(memory_usage='deep')}")
+        if self.verbose:
+            print(f"dataframe memory: {self.resultant_dataframe.info(memory_usage='deep')}")
         gc.collect()
     
     def list_files(self)->list[str]:
         return os.listdir(self.folder_path)
 
-    def load_zipped_data(self, verbose:bool)->None:
+    def load_zipped_data(self)->None:
         files = self.list_files()
 
         for file in files:
             data = None
             if file.endswith('.tsv.gz'):
-                data = self.read_files(filename=file, verbose=verbose)
+                data = self.read_files(filename=file)
             else:
                 continue
             print(f"Data from {self.folder_path}/{file}")
             
-            if verbose:
+            if self.verbose:
                 self.analyze_dataframe(data)
 
             # read these files as dataframe and concatenate them to resultant dataframe
